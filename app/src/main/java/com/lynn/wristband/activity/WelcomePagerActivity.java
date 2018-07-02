57d4ddddb8476c550a6fd764b9a4f4f0a4cc7e05
lynn8570 <lynn8570@gmail.com>
2018-07-02 17:59:58 +0800

    add pager

--- app/src/main/java/com/lynn/wristband/activity/WelcomePagerActivity.java
+++ app/src/main/java/com/lynn/wristband/activity/WelcomePagerActivity.java
@@ -5,19 +5,38 @@ import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.lynn.wristband.R;
+import com.lynn.wristband.adapter.SliderPagerAdapter;
 import com.lynn.wristband.base.FullScreenBaseActivity;
 
-public class WelcomePagerActivity extends FullScreenBaseActivity{
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class WelcomePagerActivity extends FullScreenBaseActivity {
+
+
+    @BindView(R.id.welcome_pager)
+    ViewPager mViewPager;
+    @BindView(R.id.enter_button)
+    Button mEnterButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ButterKnife.bind(this);
+
+        initViewPager();
     }
 
     @Override
@@ -25,5 +44,30 @@ public class WelcomePagerActivity extends FullScreenBaseActivity{
         return R.layout.activity_welcome_pager;
     }
 
+
+    private void initViewPager() {
+        mViewPager.setAdapter(new SliderPagerAdapter(this));
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (position == 2) {
+                    mEnterButton.setVisibility(View.VISIBLE);
+                } else {
+                    mEnterButton.setVisibility(View.GONE);
+                }
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
 
 }
---
+++ app/src/main/java/com/lynn/wristband/adapter/SliderPagerAdapter.java
@@ -0,0 +1,75 @@
+package com.lynn.wristband.adapter;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.lynn.wristband.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by zowee-laisc on 2018/7/2.
+ */
+
+public class SliderPagerAdapter extends PagerAdapter {
+    private List<View> mList;
+    private Context mContext;
+
+    public SliderPagerAdapter(Context context) {
+        this.mContext = context;
+        initViewList();
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        Log.i("linlian", "view =" + view + " object=" + object);
+        return view == object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        container.addView(mList.get(position));
+        return mList.get(position);
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView(mList.get(position));
+    }
+
+    private void initViewList() {
+        mList = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            ImageView imageView = new ImageView(mContext);
+            if(i==0){
+                imageView.setImageResource(R.drawable.cat);
+            }else if(i==1){
+                imageView.setImageResource(R.drawable.cat2);
+            }else {
+                imageView.setImageResource(R.drawable.ghost);
+            }
+
+            mList.add(imageView);
+        }
+
+    }
+
+}
